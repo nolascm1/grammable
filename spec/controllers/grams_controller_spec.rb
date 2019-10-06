59ab023d106e6f219097d968ecfea3452978da95
Melissa <nolascm1@gmail.com>
2019-10-06 22:27:01 +0000

    Lesson 12

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#show action" do
+    it "should successfully show the page if the gram is found" do
+      gram = FactoryBot.create(:gram)
+      get :show, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error if the gram is not found" do
+      get :show, params: { id: 'TACOCAT' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+
 describe "grams#index action" do
  it "should successfully show the page" do
     get :index
@@ -16,11 +30,7 @@ describe "grams#index action" do
 
 
   it "should successfully show the new form" do
-    user = User.create(
-      email:                 'fakeuser@gmail.com',
-      password:              'secretPassword',
-      password_confirmation: 'secretPassword'
-    )
+    user = FactoryBot.create(:user)
     sign_in user
    
     get :new
@@ -36,11 +46,7 @@ describe "grams#index action" do
   end
   
   it "should successfully create a new gram in our database" do
-    user = User.create(
-      email:                 'fakeuser@gmail.com',
-      password:              'secretPassword',
-      password_confirmation: 'secretPassword'
-    )
+    user = FactoryBot.create(:user)
     sign_in user
     
     post :create, params: { gram: { message: 'Hello!' } }
@@ -52,11 +58,7 @@ describe "grams#index action" do
   end
 
   it "should properly deal with validation errors" do
-    user = User.create(
-      email:                 'fakeuser@gmail.com',
-      password:              'secretPassword',
-      password_confirmation: 'secretPassword'
-    )
+    user = FactoryBot.create(:user)
     sign_in user
 
     expect(Gram.count).to eq 0
